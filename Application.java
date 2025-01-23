4fbdfb5ccb00d549b745cf04b751dee5bdece552
ganeshKallepally <ganeshkallepally127@gmail.com>
2025-01-23 13:51:18 +0530

    feature202 changes in Application.javafile by peter

--- Application.java
+++ Application.java
@@ -15,4 +15,30 @@ public class SimpleMethodExample {
         // Printing the result
         System.out.println("The sum is: " + result);
     }
+#feature202 changes by peter on user acounts module in oct2024
+class Main {
+
+  // method with no parameter
+  public void display1() {
+    System.out.println("Method without parameter");
+  }
+
+  // method with single parameter
+  public void display2(int a) {
+    System.out.println("Method with a single parameter: " + a);
+  }
+
+  public static void main(String[] args) {
+    
+    // create an object of Main
+    Main obj = new Main();
+
+    // calling method with no parameter
+    obj.display1();
+    
+    // calling method with the single parameter
+    obj.display2(24);
+  }
+}
+#end of feature202
 }
